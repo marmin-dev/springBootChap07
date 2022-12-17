acc900ca404b2e936ee26a42586af85529bc8b37
Marmin <marmin.dev@gmail.com>
2022-12-18 00:21:07 +0900

    add dependency, Change ProductControllerTest and ProdcutDto

--- src/main/java/com/springboot/jpa/data/dto/ProductDto.java
+++ src/main/java/com/springboot/jpa/data/dto/ProductDto.java
@@ -1,11 +1,13 @@
 package com.springboot.jpa.data.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class ProductDto {
     private String name;
     private int price;
